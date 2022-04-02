b84e7983ad343da62ab779fa8cf3043fa370a2d4
manson112 <rlarhksdn777@naver.com>
2022-04-02 23:59:54 +0900

    add

--- ClimbingStairs.py
+++ ClimbingStairs.py
@@ -1,3 +1,11 @@
+import math
 class Solution:
     def climbStairs(self, n: int) -> int:
-        
+        result = 1
+        j = 1
+        for i in range(n-1, n // 2 - 1, -1):
+            if i < j :
+                break
+            result += math.comb(i, j)
+            j += 1
+        return result
