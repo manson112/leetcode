60b9175a20ad7146426ac7c64eb4588e9e84b4f8
manson112 <rlarhksdn777@naver.com>
2022-04-15 00:04:30 +0900

    Change Answer

--- HappyNumber.py
+++ HappyNumber.py
@@ -1,12 +1,18 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
+        dup = {}
         while True:
             s = 0
             while n > 0:
                 num = n % 10
                 s += num * num
                 n = n // 10
+            if s in dup:
+                return False
             if s == 1:
                 return True
+            dup[s] = True
             n = s
+            
         
+print(Solution().isHappy(19))
