13f19fbc3115c4d61324eb13aab35fbae01ff71d
김관우 (Sam Kim) <rlarhksdn95@gmail.com>
2022-08-21 00:14:20 +0900

    Time: 4288 ms (5.29%), Space: 17.4 MB (6.36%) - LeetHub

--- 1995-count-special-quadruplets/1995-count-special-quadruplets.py
+++ 1995-count-special-quadruplets/1995-count-special-quadruplets.py
@@ -1,9 +1,3 @@
 class Solution:
     def countQuadruplets(self, nums: List[int]) -> int:
-        counter = Counter(nums)
-        coms = itertools.combinations(nums, 4)
-        result = 0
-        for c in coms:
-            if sum(c[:3]) == c[3]:
-                result += 1
-        return result      
+        return sum([1 if sum(c[:3]) == c[3] else 0 for c in itertools.combinations(nums, 4)])
