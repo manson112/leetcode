4b5435cfdd579055ea31df9c62021a1a9083607f
김관우 (Sam Kim) <rlarhksdn95@gmail.com>
2022-08-20 23:33:49 +0900

    Time: 61 ms (23.01%), Space: 13.8 MB (94.69%) - LeetHub

--- 2027-minimum-moves-to-convert-string/2027-minimum-moves-to-convert-string.py
+++ 2027-minimum-moves-to-convert-string/2027-minimum-moves-to-convert-string.py
@@ -2,16 +2,13 @@ class Solution:
     def minimumMoves(self, s: str) -> int:
         result = 0
         i = 0
-        while i < len(s)-3:
+        while i < len(s):
             if s[i] == 'X':
                 result += 1
                 i += 3
             else:
                 i += 1
-        for j in range(len(s)-1, i-1, -1):
-            if s[j] == 'X':
-                result += 1
-                break
+      
     
         return result
                 
