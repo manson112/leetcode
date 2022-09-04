65914587c71285db6c043cdf7c8e386dae610e9f
김관우 (Sam Kim) <rlarhksdn95@gmail.com>
2022-09-04 17:16:18 +0900

    Time: 47 ms (88.81%), Space: 13.9 MB (32.62%) - LeetHub

--- 1854-maximum-population-year/1854-maximum-population-year.py
+++ 1854-maximum-population-year/1854-maximum-population-year.py
@@ -1,14 +1,17 @@
 class Solution:
     def maximumPopulation(self, logs: List[List[int]]) -> int:
+        m = 0
+        year = 0
         arr = [0 for i in range(101)]
         for log in logs:
             for i in range(log[0], log[1]):
                 arr[i - 1950] += 1
+                if arr[i-1950] > m:
+                    year = i
+                    m = arr[i-1950]
+                elif arr[i-1950] == m:
+                    if i < year:
+                        year = i
                 
-        m = 0
-        year = 0
-        for i, v in enumerate(arr):
-            if v > m:
-                m = v
-                year = i + 1950
+                
         return year
