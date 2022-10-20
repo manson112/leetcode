ac78417faec608b62ef26b9e0796307957f10356
manson112 <rlarhksdn777@naver.com>
2022-10-21 00:13:01 +0900

    1587

---
+++ 1587.py
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+select a.customer_id, count(a.visit_id) as count_no_trans
+  from Visits a 
+  left join Transactions b 
+    on a.visit_id = b.visit_id
+  where b.transaction_id is null
+  group by a.customer_id
+  order by count_no_trans desc, a.customer_id
